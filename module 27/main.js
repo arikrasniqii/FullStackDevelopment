03d52206a8b2849d69d437abf403791c8118328e
arikrasniqii <ariikrasniqi@gmail.com>
2026-05-19 20:42:09 +0200

    module 27

---
+++ module 27/main.js
@@ -0,0 +1,35 @@
+function showmessage(){
+    alert('This is my first function')
+}
+
+showmessage()
+
+function toCelsius (Farhenheit){
+    return(5/9) * (Farhenheit - 32)
+}
+
+
+var result = toCelsius(54)
+console.log("Farhenheit in celsius is "+result+ "degree")
+
+
+function sum(numri1, numri2){
+    return numri1 + numri2
+}
+console.log(sum(32,42))
+
+function sub(numri1, numri2){
+    return numri1 - numri2
+}
+console.log(sub(32,42))
+
+function multi(numri1, numri2){
+    return numri1 * numri2
+}
+console.log(multi(32,42))
+
+function div(numri1, numri2){
+    return numri1 / numri2
+}
+console.log(div(32,42))
+
