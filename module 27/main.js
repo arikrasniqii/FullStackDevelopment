4654f5b51379731cf56b2ad99afed358cd93be19
arikrasniqii <ariikrasniqi@gmail.com>
2026-05-21 20:51:48 +0200

    27

--- module 27/main.js
+++ module 27/main.js
@@ -33,3 +33,45 @@ function div(numri1, numri2){
 }
 console.log(div(32,42))
 
+function digitalschool(){
+    var shkolla = 'DigitalSchool';
+    alert(shkolla)
+}
+
+digitalschool()
+
+function siperfaqja_e_trekendeshit(baza,lartesia){
+    return baza * lartesia * 0.5
+}
+
+console.log('Siperfaqja e trekendeshit eshte: '+siperfaqja_e_trekendeshit(5,7))
+
+var audia = {
+    type: 'RS6',
+    color: 'Black',
+    viti_i_prodhimit:2023,
+    kilometra:120.000,
+    engine:function(){
+        alert('Vroommmm')
+    },
+    get getkilometra(){
+        return this.kilometra
+
+    },
+
+    set setkilometra(km){
+        this.kilometra = km
+
+    }
+
+
+}
+
+alert(audia.type)
+
+
+
+audia.engine()
+console.log(audia.getkilometra)
+audia.setkilometra = 180;
+console.log(audia.setkilometra)
